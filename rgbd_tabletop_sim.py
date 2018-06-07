1378c7f9db165cbb24bad6dd077157f83cb1f557
Gregory Izatt <gregory.izatt@gmail.com>
2018-06-07 18:05:23 -0400

    Add rectification

--- rgbd_tabletop_sim.py
+++ rgbd_tabletop_sim.py
@@ -2,6 +2,7 @@
 
 import argparse
 import os
+import random
 import time
 
 import cv2
@@ -60,7 +61,7 @@ def save_image_colormap(name, im):
 
 def save_depth_colormap(name, im, near, far):
     cmapped = plt.cm.jet((far - im)/(far - near))
-    zero_range_mask = im < near #np.repeat((im < near)[:, :, np.newaxis], 4, axis=2)
+    zero_range_mask = im < near
     cmapped[:, :, 0][zero_range_mask] = 0.0
     cmapped[:, :, 1][zero_range_mask] = 0.0
     cmapped[:, :, 2][zero_range_mask] = 0.0
@@ -108,7 +109,6 @@ def setup_tabletop(rbt):
 
     table_top_z_in_world = 0.736 + 0.057 / 2
 
-    np.random.seed(42)
     for i in range(len(object_urdf_paths)):
         hp = (np.random.random(2)-0.5)*0.25
         object_init_frame = RigidBodyFrame(
@@ -144,11 +144,15 @@ def setup_tabletop(rbt):
 
 
 class DepthImageCorruptionBlock(LeafSystem):
-    def __init__(self,
-                 camera):
+    def __init__(self, camera, save_dir):
         LeafSystem.__init__(self)
         self.set_name('depth image corruption superclass')
         self.camera = camera
+
+        self.save_dir = save_dir
+        if self.save_dir is not None:
+            os.system("rm -r %s" % self.save_dir)
+            os.system("mkdir -p %s" % self.save_dir)
 
         self.depth_image_input_port = \
             self._DeclareInputPort(PortDataType.kAbstractValued,
@@ -167,7 +171,6 @@ class DepthImageCorruptionBlock(LeafSystem):
         test = AbstractValue.Make(Image[PixelType.kDepth32F](
             self.camera.depth_camera_info().width(),
             self.camera.depth_camera_info().height()))
-        print "Allocated ", test
         return test
 
     def _DoCalcAbstractOutput(self, context, y_data):
@@ -176,25 +179,28 @@ class DepthImageCorruptionBlock(LeafSystem):
 
 
 class DepthImageHeuristicCorruptionBlock(DepthImageCorruptionBlock):
-    def __init__(self,
-                 camera):
-        DepthImageCorruptionBlock.__init__(self, camera)
+    def __init__(self, camera, save_dir):
+        DepthImageCorruptionBlock.__init__(self, camera, save_dir)
         self.set_name('depth image corruption, heuristic')
 
-        self.rgbd_normal_limit = 0.5
-        self.rgbd_noise = 0.001
+        self.rgbd_normal_limit = 0.0
+        self.rgbd_noise = 0.000
         self.rgbd_projector_baseline = 0.05
+        self.rgbd_rectification_baseline = -0.025
         self.near_distance = 0.2
         self.far_distance = 3.5
 
         # Cache these things that are used in every loop
         # to minimize re-allocation of these big arrays
         K = self.camera.depth_camera_info().intrinsic_matrix()
+        K_rgb = self.camera.color_camera_info().intrinsic_matrix()
         w = self.camera.depth_camera_info().width()
         h = self.camera.depth_camera_info().height()
         # How much does each depth point project laterally
         # (in the axis of the camera-projector pair?)
-        self.x_indices_im = np.tile(np.arange(w), [h, 1])
+        x_inds, y_inds = np.meshgrid(np.arange(w), np.arange(h))
+        self.xy1_indices_im = np.dstack([
+            x_inds, y_inds, np.ones((h, w))])
         self.iter = 0
 
     def _DoCalcAbstractOutput(self, context, y_data):
@@ -204,8 +210,10 @@ class DepthImageHeuristicCorruptionBlock(DepthImageCorruptionBlock):
         h, w, _ = u_data.data.shape
         rgb_image = np.empty((h, w), dtype=np.float64)
         rgb_image[:, :] = u_data.data[:, :, 0]
-        save_image_uint8("images/%05d_rgb.png" % self.iter,
-                         rgb_image)
+
+        if self.save_dir is not None:
+            save_image_uint8(
+                "%s/%05d_rgb.png" % (self.save_dir, self.iter), rgb_image)
 
         u_data = self.EvalAbstractInput(context, 0).get_value()
         h, w, _ = u_data.data.shape
@@ -215,9 +223,10 @@ class DepthImageHeuristicCorruptionBlock(DepthImageCorruptionBlock):
         depth_image = np.clip(depth_image, self.near_distance,
                               self.far_distance)
 
-        save_depth_colormap("images/%05d_input_depth.png" % self.iter,
-                            depth_image,
-                            self.near_distance, self.far_distance)
+        if self.save_dir is not None:
+            save_depth_colormap(
+                "%s/%05d_input_depth.png" % (self.save_dir, self.iter),
+                depth_image, self.near_distance, self.far_distance)
 
         # Calculate normals before adding noise
         if self.rgbd_normal_limit > 0.:
@@ -232,18 +241,15 @@ class DepthImageHeuristicCorruptionBlock(DepthImageCorruptionBlock):
             noiseMat = np.random.randn(h, w)*self.rgbd_noise
             depth_image += noiseMat
 
-        depth_image_out = np.zeros(depth_image.shape)
-        depth_image_out += depth_image
-
         if self.rgbd_projector_baseline > 0.0:
 
             K = self.camera.depth_camera_info().intrinsic_matrix()
-            x_projection = (self.x_indices_im - K[0, 2]) * \
+            x_projection = (self.xy1_indices_im[:, :, 0] - K[0, 2]) * \
                 depth_image / K[0, 0]
 
             # For a fixed shift...
-            mask = np.ones(depth_image_out.shape)
-            for shift_amt in range(-50, 0, 1):
+            mask = np.ones(depth_image.shape)
+            for shift_amt in range(-50, 0, 10):
                 imshift_tf_matrix = np.array(
                     [[1., 0., shift_amt], [0., 1., 0.]])
                 sh_x_projection = cv2.warpAffine(
@@ -251,8 +257,7 @@ class DepthImageHeuristicCorruptionBlock(DepthImageCorruptionBlock):
                     (w, h), borderMode=cv2.BORDER_REPLICATE)
                 shifted_gt_depth = cv2.warpAffine(
                     depth_image, imshift_tf_matrix, (w, h),
-                    borderMode=cv2.BORDER_CONSTANT,
-                    borderValue=float(np.max(depth_image)))
+                    borderMode=cv2.BORDER_REPLICATE)
 
                 # (projected test point - projected original point) dot
                 # producted with vector perpendicular to sample point
@@ -267,35 +272,93 @@ class DepthImageHeuristicCorruptionBlock(DepthImageCorruptionBlock):
                     error_im.astype(np.float32), 0., 1.,
                     cv2.THRESH_BINARY_INV)
                 mask *= error_thresh
-            depth_image_out *= mask
-            save_image_colormap("images/%05d_mask.png" % self.iter,
-                                mask)
-            save_depth_colormap("images/%05d_masked_depth.png" % self.iter,
-                                depth_image_out, self.near_distance,
-                                self.far_distance)
+            depth_image *= mask
+
+            if self.save_dir is not None:
+                save_image_colormap(
+                    "%s/%05d_mask.png" % (self.save_dir, self.iter), mask)
+                save_depth_colormap(
+                    "%s/%05d_prerectified_masked_depth.png" % (
+                        self.save_dir, self.iter),
+                    depth_image, self.near_distance,
+                    self.far_distance)
 
         # Apply normal limiting
         if self.rgbd_normal_limit > 0.:
-            depth_image_out *= normalThresh
+            depth_image *= normalThresh
+
+        # And finally apply rectification to RGB frame
+        if self.rgbd_rectification_baseline != 0.:
+            # Convert depth image to point cloud, with +z being
+            # camera "forward"
+            K = self.camera.depth_camera_info().intrinsic_matrix()
+            K_rgb = self.camera.color_camera_info().intrinsic_matrix()
+            Kinv = np.linalg.inv(K)
+            U, V = np.meshgrid(np.arange(h), np.arange(w))
+            points_in_camera_frame = np.vstack([
+                U.T.flatten(),
+                V.T.flatten(),
+                np.ones(w*h)])
+            points_in_camera_frame = Kinv.dot(points_in_camera_frame) * \
+                depth_image.flatten()
+            # Shift them over into the rgb camera frame
+            # points_in_camera_frame[0, :] += self.rgbd_rectification_baseline
+            # Reproject back into the the image (using the RGB
+            # projection matrix. This is wrong (and very bad) if the
+            # resolution of the RGB and Depth are different.
+            points_in_camera_frame[1, :] += self.rgbd_rectification_baseline
+            points_in_camera_frame[0, :] /= points_in_camera_frame[2, :]
+            points_in_camera_frame[1, :] /= points_in_camera_frame[2, :]
+            points_in_camera_frame[2, :] /= points_in_camera_frame[2, :]
+            depth_image_out = np.full(depth_image.shape, np.inf)
+            reprojected_uv = K_rgb.dot(points_in_camera_frame)
+            for u in range(h):
+                for v in range(w):
+                    if not np.isfinite(reprojected_uv[2, u*w+v]):
+                        #print "rejecting ", u, v, reprojected_uv[:, v*h+u]
+                        continue
+                    proj_vu = np.round(reprojected_uv[:, u*w+v]).astype(int)
+                    if (proj_vu[0] >= 0 and proj_vu[0] < h and
+                            proj_vu[1] >= 0 and proj_vu[1] < w):
+                        depth_image_out[proj_vu[0], proj_vu[1]] = (
+                            min(depth_image_out[proj_vu[0], proj_vu[1]],
+                                depth_image[u, v]))
+            # Resaturate infs to 0
+            depth_image_out[np.isinf(depth_image_out)] = 0.
+
+        else:
+            depth_image_out = depth_image
+
+        save_depth_colormap(
+            "%s/%05d_masked_depth.png" % (
+                self.save_dir, self.iter),
+            depth_image_out, self.near_distance,
+            self.far_distance)
 
         # Where it's infinite, set to 0
         depth_image_out = np.where(
             good_mask, depth_image_out,
             np.zeros(depth_image.shape))
+
         y_data.get_mutable_value().mutable_data[:, :, 0] = \
             depth_image_out[:, :]
         print "Elapsed in render (model): %f seconds" % \
             (time.time() - start_time)
         self.iter += 1
 
+
 class DepthImageCNNCorruptionBlock(DepthImageCorruptionBlock):
-    def __init__(self,
-                 camera):
-        DepthImageCorruptionBlock.__init__(self, camera)
+    def __init__(self, camera, save_dir, single_scene_mode=False):
+        DepthImageCorruptionBlock.__init__(self, camera, save_dir)
         self.set_name('depth image corruption, cnn')
 
-        self.model = network.load_trained_model(
-            weights_path="DepthSim/python/models/net_depth_seg_v1.hdf5")
+        if single_scene_mode:
+            self.model = network.load_trained_model(
+                weights_path="DepthSim/python/models/"
+                "2017-06-16-30_depth_batch4.hdf5")
+        else:
+            self.model = network.load_trained_model(
+                weights_path="DepthSim/python/models/net_depth_seg_v1.hdf5")
 
         self.near_distance = 0.2
         self.far_distance = 3.5
@@ -309,8 +372,10 @@ class DepthImageCNNCorruptionBlock(DepthImageCorruptionBlock):
         h, w, _ = u_data.data.shape
         rgb_image = np.empty((h, w), dtype=np.float64)
         rgb_image[:, :] = u_data.data[:, :, 0]
-        save_image_uint8("images/%05d_rgb.png" % self.iter,
-                         rgb_image)
+
+        if self.save_dir is not None:
+            save_image_uint8(
+                "%s/%05d_rgb.png" % (self.save_dir, self.iter), rgb_image)
 
         u_data = self.EvalAbstractInput(context, 0).get_value()
         h, w, _ = u_data.data.shape
@@ -324,14 +389,19 @@ class DepthImageCNNCorruptionBlock(DepthImageCorruptionBlock):
         depth_image_resized = cv2.resize(
             depth_image_normalized, (640, 480),
             interpolation=cv2.INTER_NEAREST)
-        save_depth_colormap("images/%05d_input_depth.png" % self.iter,
-                            depth_image_resized,
-                            self.near_distance/self.far_distance, 1.0)
+
+
         stack = np.empty((1, 480, 640, 1))
         stack[0, :, :, 0] = depth_image_resized[:, :]
         predicted_prob_map = self.model.predict_on_batch(stack)
-        save_image_colormap("images/%05d_mask.png" % self.iter,
-                            predicted_prob_map[0, :, :, 0])
+
+        if self.save_dir is not None:
+            save_depth_colormap(
+                "%s/%05d_input_depth.png" % (self.save_dir, self.iter),
+                depth_image_resized, self.near_distance/self.far_distance, 1.0)
+            save_image_colormap(
+                "%s/%05d_mask.png" % (self.save_dir, self.iter),
+                predicted_prob_map[0, :, :, 0])
 
         depth_image_resized[predicted_prob_map[0, :, :, 0] >
                             self.dropout_threshold] = 0.
@@ -350,7 +420,7 @@ class DepthImageCNNCorruptionBlock(DepthImageCorruptionBlock):
         #                  interpolation=cv2.INTER_LINEAR)
         #depth_image_resized[mask > 0.5] = 0.
 
-        #save_image_colormap("images/%05d_noise_matrix.png" % self.iter,
+        #save_image_colormap("%s/%05d_noise_matrix.png" % (self.save_dir, self.iter),
         #                    mask)
         #network.apply_mask(predicted_prob_map, depth_image_resized,
         #                   self.dropout_threshold)
@@ -359,8 +429,12 @@ class DepthImageCNNCorruptionBlock(DepthImageCorruptionBlock):
         #    depth_image_resized,
         #    np.zeros(depth_image_resized.shape))
         depth_image = self.far_distance * depth_image_resized
-        save_depth_colormap("images/%05d_masked_depth.png" % self.iter,
-                            depth_image, self.near_distance, self.far_distance)
+
+        if self.save_dir is not None:
+            save_depth_colormap(
+                "%s/%05d_masked_depth.png" % (self.save_dir, self.iter),
+                depth_image, self.near_distance, self.far_distance)
+
         # Where it's infinite, set to 0
         depth_image = np.where(
             good_mask, depth_image,
@@ -465,8 +539,21 @@ if __name__ == "__main__":
     parser.add_argument("--corruption_method",
                         type=str,
                         default="cnn",
-                        help="[cnn, model, none]")
+                        help="[cnn, cnn_single_scene, model, none]")
+    parser.add_argument("--seed",
+                        type=int,
+                        default=42,
+                        help="Random seed for rng, "
+                             "including scene generation.")
+    parser.add_argument("--save_dir",
+                        type=str,
+                        default=None,
+                        help="Directory to save depth diagnostic images."
+                             " If not specified, does not save images.")
     args = parser.parse_args()
+
+    np.random.seed(args.seed)
+    random.seed(args.seed)
 
     meshcat_server_p = None
     if args.test:
@@ -524,15 +611,19 @@ if __name__ == "__main__":
 
     if args.corruption_method == "model":
         depth_corruptor = builder.AddSystem(
-            DepthImageHeuristicCorruptionBlock(camera))
+            DepthImageHeuristicCorruptionBlock(camera, args.save_dir))
         builder.Connect(camera.color_image_output_port(),
                         depth_corruptor.color_image_input_port)
         builder.Connect(camera.depth_image_output_port(),
                         depth_corruptor.depth_image_input_port)
         final_depth_output_port = depth_corruptor.depth_image_output_port
-    elif args.corruption_method == "cnn":
+    elif (args.corruption_method == "cnn" or
+          args.corruption_method == "cnn_single_scene"):
         depth_corruptor = builder.AddSystem(
-            DepthImageCNNCorruptionBlock(camera))
+            DepthImageCNNCorruptionBlock(
+                camera, args.save_dir,
+                single_scene_mode=(
+                    args.corruption_method == "cnn_single_scene")))
         builder.Connect(camera.color_image_output_port(),
                         depth_corruptor.color_image_input_port)
         builder.Connect(camera.depth_image_output_port(),
